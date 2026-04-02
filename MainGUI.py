d2ba9e99ae55e0f6118962eb5a3b798d41678fbe
Jakub Czuba <jczuba@wsb.edu.pl>
2026-04-02 13:40:47 +0200

    New Window + add HUBI LPG filess to GUI folder

---
+++ MainGUI.py
@@ -0,0 +1,11 @@
+import customtkinter
+
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("blue")
+
+GUI = customtkinter.CTk()
+
+GUI.title ("WL-Maker3.0")
+GUI.geometry("1000x500")
+
+GUI.mainloop()
---
+++ README.md
@@ -0,0 +1,2 @@
+# Robimy wlki bratku 🥵🥵🥵🥵🥵🥵🥵
+Do wyeksportowania do exe wpisz to w cmd: pyinstaller --noconfirm --onedir --console wl_maker.py
